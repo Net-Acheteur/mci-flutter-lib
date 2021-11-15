7d3a8d7a569695ab601fe97f03f7f111353816e2
Vincent <vderousiers@monchasseurimmo.com>
2021-11-15 14:24:07 +0100

    DRWN-1831 Init

---
+++ example/mci_flutter_lib_example.dart
@@ -0,0 +1,6 @@
+import 'package:mci_flutter_lib/config/mci_colors.dart';
+
+void main() {
+  var color = MCIColors.primary;
+  print('Primary color: ${color.toString()}');
+}
---
+++ lib/mci_flutter_lib.dart
@@ -0,0 +1,4 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mci_flutter_lib;
