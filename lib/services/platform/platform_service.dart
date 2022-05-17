9c777c32081b5fe87f1a10b667541f55b147bad1
Vincent <vderousiers@monchasseurimmo.com>
2022-05-17 11:16:42 +0200

    DRWN-2480 Dumb

--- lib/services/platform/platform_service.dart
+++ lib/services/platform/platform_service.dart
@@ -1,3 +1,4 @@
-export 'package:mci_flutter_lib/services/platform/platform_service_mobile.dart'
+export 'package:mci_flutter_lib/services/platform/platform_service_stub.dart'
+    if (dart.library.io) 'package:mci_flutter_lib/services/external_link/external_link_service_mobile.dart'
     if (dart.library.html) 'package:mci_flutter_lib/services/external_link/external_link_service_web.dart'
     show PlatformService;
